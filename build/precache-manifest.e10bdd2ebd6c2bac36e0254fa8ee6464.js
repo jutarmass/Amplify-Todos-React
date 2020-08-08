22cc4aaf330b9f8f42094f38b68ec8d0cd4e42ea
jutarmass <jkhongjang@gmail.com>
2020-08-08 10:13:57 +0700

    re-build

---
+++ build/precache-manifest.e10bdd2ebd6c2bac36e0254fa8ee6464.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae5c07684f1f832b43fcbfdbeb37826",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4f72fdc818fb1b81034",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "144d5345aced959a3c89",
+    "url": "/static/js/2.640109ee.chunk.js"
+  },
+  {
+    "revision": "2484cd6bcea4995b92934619b09cb02d",
+    "url": "/static/js/2.640109ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f72fdc818fb1b81034",
+    "url": "/static/js/main.004a2004.chunk.js"
+  },
+  {
+    "revision": "68b53d1dbdff68322314",
+    "url": "/static/js/runtime-main.b8d21cf4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
